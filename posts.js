fb92ebf0d7db9d0b45f973010d83316f841081df
dwaynediddy <burrows.dwayne@icloud.com>
2022-08-18 13:03:55 +0200

    editted the json data

--- posts.js
+++ posts.js
@@ -3,234 +3,120 @@ module.exports = [
       "_id": "62fe0e9e66cb471052f066e6",
       "index": 0,
       "picture": "http://placehold.it/32x32",
-      "name": "Perry Levy",
+      "name": "D-train",
       "gender": "male",
-      "email": "perrylevy@sultraxin.com",
+      "email": "D-swizzle@mcDonalds.com",
       "phone": "+1 (909) 534-2482",
       "address": "835 Williams Place, Succasunna, Maryland, 673",
       "about": "Et est culpa nulla culpa irure velit ut occaecat non dolore aliqua. Excepteur consectetur duis dolor irure dolor excepteur esse pariatur incididunt ipsum. Sit ea pariatur mollit minim dolor do adipisicing laborum ullamco laboris nostrud in veniam. Sit aliqua eiusmod excepteur laboris anim dolore amet sunt anim nostrud. Enim adipisicing ad dolor ipsum pariatur nostrud. Incididunt cupidatat cillum eiusmod labore aute laboris est proident cupidatat in qui reprehenderit velit.\r\n",
       "registered": "2020-06-20T04:40:38 -02:00",
       "latitude": 6.751029,
       "longitude": 78.990006,
-      "tags": [
-        "sunt",
-        "nulla",
-        "laboris",
-        "anim",
-        "Lorem",
-        "adipisicing",
-        "laboris"
-      ],
-      "friends": [
+      "quotes": [
         {
           "id": 0,
-          "name": "Best Mcfarland"
+          "quote": "Would you like frys with that?"
         },
         {
           "id": 1,
-          "name": "Humphrey Justice"
+          "quote": "Bi Zui Dong bu dong"
         },
         {
           "id": 2,
-          "name": "Austin Frazier"
+          "quote": "Bad mamajama"
         }
       ],
-      "greeting": "Hello, Perry Levy! You have 8 unread messages.",
-      "favoriteFruit": "strawberry"
+      "greeting": "Hello, D-Swiz! You have 8 unread messages.",
+      "favoriteFruit": "Kiwi"
     },
     {
       "_id": "62fe0e9e33cb32f230df04c2",
       "index": 1,
       "picture": "http://placehold.it/32x32",
-      "name": "Shanna Henson",
+      "name": "Liv",
       "gender": "female",
-      "email": "shannahenson@sultraxin.com",
+      "email": "livnerdclouddev@cloud.com",
       "phone": "+1 (998) 534-3117",
       "address": "350 Henderson Walk, Adelino, Virginia, 9991",
       "about": "Tempor culpa nisi fugiat aliquip dolore ut magna qui duis. Ex minim mollit adipisicing qui eiusmod irure qui consequat officia eu et in non cillum. Laborum est in sint dolore in nostrud veniam adipisicing nulla qui. Incididunt ut dolore exercitation nulla reprehenderit adipisicing commodo officia adipisicing minim eu enim. Adipisicing pariatur excepteur deserunt ullamco excepteur reprehenderit laboris non. Velit aliqua nisi ipsum magna dolore quis pariatur. Magna mollit nisi exercitation qui velit est aute.\r\n",
       "registered": "2018-03-18T12:17:46 -01:00",
       "latitude": -29.064993,
       "longitude": 112.999148,
-      "tags": [
-        "ea",
-        "proident",
-        "irure",
-        "sint",
-        "ad",
-        "sit",
-        "nostrud"
-      ],
-      "friends": [
+      "quotes": [
         {
           "id": 0,
-          "name": "West Miranda"
+          "quote": "I just got domed by D-swizzle, Deagle ... again!"
         },
         {
           "id": 1,
-          "name": "Lorrie Kane"
+          "quote": "哈哈哈哈哈哈哈哈哈哈"
         },
         {
           "id": 2,
-          "name": "Coleman Butler"
+          "quote": "Catfish"
         }
       ],
-      "greeting": "Hello, Shanna Henson! You have 2 unread messages.",
+      "greeting": "Hello, liv! You have 2 unread messages.",
       "favoriteFruit": "strawberry"
     },
     {
       "_id": "62fe0e9e8da887fd6180234b",
       "index": 2,
       "picture": "http://placehold.it/32x32",
-      "name": "Swanson Padilla",
-      "gender": "male",
-      "email": "swansonpadilla@sultraxin.com",
+      "name": "nyan cat",
+      "gender": "kot",
+      "email": "nyannyannyanyanyan@cat.com",
       "phone": "+1 (993) 579-3367",
       "address": "707 Chester Court, Edgar, California, 1306",
       "about": "Labore non elit consequat dolore. Id duis aliquip non sunt ad minim qui. Enim enim commodo non deserunt elit nostrud exercitation nulla aute dolor do non fugiat ullamco. Cupidatat voluptate veniam magna deserunt elit reprehenderit Lorem reprehenderit duis laboris aliquip non occaecat. Lorem elit voluptate non amet tempor veniam ea ea ut. Cillum esse sunt velit aliquip laborum qui sunt. Aliqua occaecat velit in esse ipsum mollit esse voluptate anim excepteur officia cupidatat.\r\n",
       "registered": "2018-07-23T07:06:38 -02:00",
       "latitude": -65.987717,
       "longitude": 47.69105,
-      "tags": [
-        "culpa",
-        "cupidatat",
-        "pariatur",
-        "consectetur",
-        "exercitation",
-        "exercitation",
-        "mollit"
-      ],
-      "friends": [
+      "quotes": [
         {
           "id": 0,
-          "name": "Rosales Blackwell"
+          "quote": "Nyan Nyan Nyan Nyan Nyan Nyan Nyan"
         },
         {
           "id": 1,
-          "name": "Willa Calderon"
+          "quote": "meow"
         },
         {
           "id": 2,
-          "name": "Alexander Burnett"
+          "quote": "Nyan"
         }
       ],
-      "greeting": "Hello, Swanson Padilla! You have 7 unread messages.",
+      "greeting": "Hello, kot! You have 7 unread messages.",
       "favoriteFruit": "apple"
     },
     {
       "_id": "62fe0e9e4eb6ff1947f6022a",
       "index": 3,
       "picture": "http://placehold.it/32x32",
-      "name": "Blake Simmons",
+      "name": "Dwight Schrute",
       "gender": "male",
-      "email": "blakesimmons@sultraxin.com",
+      "email": "Dwight@shrutefarms.com",
       "phone": "+1 (883) 553-2825",
       "address": "379 Wakeman Place, Fedora, Nevada, 9819",
       "about": "Anim officia mollit sint exercitation ad ad adipisicing proident deserunt laboris. Ex ullamco incididunt consectetur est eu aliqua anim officia ullamco laborum. Cupidatat excepteur ex qui nostrud nisi minim esse non voluptate aliqua.\r\n",
       "registered": "2014-04-24T06:07:12 -02:00",
       "latitude": -24.72496,
       "longitude": 7.55674,
-      "tags": [
-        "pariatur",
-        "anim",
-        "deserunt",
-        "ut",
-        "irure",
-        "officia",
-        "Lorem"
-      ],
-      "friends": [
+      "quotes": [
         {
           "id": 0,
-          "name": "Mitchell Ingram"
+          "quote": "I am assistant regional manager"
         },
         {
           "id": 1,
-          "name": "Lakeisha Benton"
+          "quote": "Whenever i am about to do something i ask myself, would an idiot do that?"
         },
         {
           "id": 2,
-          "name": "Margo Pickett"
+          "quote": "5 minutes ahead of schedule, right on schedule"
         }
       ],
-      "greeting": "Hello, Blake Simmons! You have 6 unread messages.",
+      "greeting": "Hello, assistant To the regional manager! You have 6 unread messages.",
       "favoriteFruit": "strawberry"
     },
-    {
-      "_id": "62fe0e9e57d9b1a2393598e9",
-      "index": 4,
-      "picture": "http://placehold.it/32x32",
-      "name": "Elva Rivas",
-      "gender": "female",
-      "email": "elvarivas@sultraxin.com",
-      "phone": "+1 (896) 479-3245",
-      "address": "678 Durland Place, Barstow, Illinois, 2185",
-      "about": "Adipisicing nulla dolor elit in ullamco mollit consequat tempor in consectetur in. Deserunt sunt ex aliqua qui magna laboris nisi consectetur excepteur exercitation ex non irure. Tempor dolor fugiat occaecat est. Sint duis duis laboris aute aliquip sunt excepteur culpa do aliquip ea qui minim. Incididunt do commodo consectetur minim culpa esse incididunt dolore nisi nostrud sit esse quis tempor. Qui cupidatat duis velit est id mollit dolor proident eiusmod. Id labore et reprehenderit excepteur culpa nisi nostrud Lorem est aliquip eiusmod ullamco aliqua.\r\n",
-      "registered": "2018-10-24T01:38:52 -02:00",
-      "latitude": 64.608351,
-      "longitude": -130.295816,
-      "tags": [
-        "dolore",
-        "amet",
-        "eiusmod",
-        "laborum",
-        "nisi",
-        "occaecat",
-        "non"
-      ],
-      "friends": [
-        {
-          "id": 0,
-          "name": "Robin Boyd"
-        },
-        {
-          "id": 1,
-          "name": "Mercer Tate"
-        },
-        {
-          "id": 2,
-          "name": "Audra Valenzuela"
-        }
-      ],
-      "greeting": "Hello, Elva Rivas! You have 9 unread messages.",
-      "favoriteFruit": "strawberry"
-    },
-    {
-      "_id": "62fe0e9eacc7df234d61aa6b",
-      "index": 5,
-      "picture": "http://placehold.it/32x32",
-      "name": "Booth Massey",
-      "gender": "male",
-      "email": "boothmassey@sultraxin.com",
-      "phone": "+1 (955) 420-3517",
-      "address": "717 Ira Court, Martell, Maine, 892",
-      "about": "Ullamco laboris est consectetur esse nostrud enim ad est sit duis minim. Aute reprehenderit dolor esse laborum excepteur cupidatat ipsum voluptate cupidatat anim sit ad dolore ad. Reprehenderit tempor nisi mollit laboris labore mollit id do culpa excepteur incididunt veniam. Excepteur aliqua non ea consequat et cillum ad consectetur sit adipisicing ex. Aliqua in incididunt enim veniam veniam.\r\n",
-      "registered": "2018-07-27T10:25:04 -02:00",
-      "latitude": 88.884942,
-      "longitude": 51.60488,
-      "tags": [
-        "commodo",
-        "laborum",
-        "enim",
-        "amet",
-        "laboris",
-        "exercitation",
-        "proident"
-      ],
-      "friends": [
-        {
-          "id": 0,
-          "name": "Sara Lewis"
-        },
-        {
-          "id": 1,
-          "name": "Phelps Fitzgerald"
-        },
-        {
-          "id": 2,
-          "name": "Maryanne Schroeder"
-        }
-      ],
-      "greeting": "Hello, Booth Massey! You have 1 unread messages.",
-      "favoriteFruit": "apple"
-    }
-  ]
+]
